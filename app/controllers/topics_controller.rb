b64748f95d8d813e6c150b47dd55651e395e4331
Carl Swanson <cetswanson@gmail.com>
2015-05-21 21:27:05 -0400

    Add create method to topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,15 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
   end
+
+  def create
+    user = current_user if current_user
+    topic = user.topics.build(topic_params)
+
+    if topic.save
+      redirect_to topic_path(topic)
+    else
+      redirect_to topics_path
+    end
+  end
 end
