b062e0a6fd4b2052262b247df43adb1468e0afe8
Carl Swanson <cetswanson@gmail.com>
2015-05-24 12:04:17 -0400

    Add create method to subforums controller

--- app/controllers/subforums_controller.rb
+++ app/controllers/subforums_controller.rb
@@ -7,4 +7,15 @@ class SubforumsController < ApplicationController
   def new
     @subforum = Subforum.new
   end
+
+  def create
+    user = current_user if current_user
+    @subforum = user.subforums.build(subforum_params)
+
+    if @subforum.save
+      redirect_to subforum_topics_path(@subforum)
+    else
+      redirect_to new_subforum_path
+    end
+  end
 end
