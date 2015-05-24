0cb979ef8fa4f2646a9e128cdc723d3adfbc16d0
Carl Swanson <cetswanson@gmail.com>
2015-05-24 15:27:26 -0400

    Add titleless topic creation feature test

--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -15,4 +15,15 @@ RSpec.feature "User creates a new topic", :type => :feature do
 
     expect{click_button "Submit"}.to change{Topic.count}.by(1)
   end
+
+  scenario "A user creates a new topic correctly" do
+    page.set_rack_session(user_id: user.id)
+    visit subforum_topics_path(subforum)
+    click_on "Add a topic"
+
+    fill_in "topic_description", :with => "test"
+
+    expect{click_button "Submit"}.not_to change(Topic, :count)
+  end
+
 end
