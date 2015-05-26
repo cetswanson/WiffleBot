6c0556201594ada72c74228c21548b0cb42f9504
Carl Swanson <cetswanson@gmail.com>
2015-05-26 13:25:33 -0400

    Add null false validation to subforum description

--- db/migrate/20150524050313_create_subforums.rb
+++ db/migrate/20150524050313_create_subforums.rb
@@ -2,7 +2,7 @@ class CreateSubforums < ActiveRecord::Migration
   def change
     create_table :subforums do |t|
       t.string :title, null: false
-      t.text :description
+      t.text :description, null: false
       t.references :user
 
       t.timestamps
