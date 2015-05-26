0f808ed092496392ee052777abe28f54721a5ddd
Carl Swanson <cetswanson@gmail.com>
2015-05-26 13:23:59 -0400

    Add length validation to user name

--- db/migrate/20150522001142_create_users.rb
+++ db/migrate/20150522001142_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, unique: true, null: false
       t.string :password_digest
+      t.string :name, unique: true, null: false, length: { minimum: 2 }
 
       t.timestamps
     end
