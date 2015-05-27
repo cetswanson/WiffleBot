8bb2b506b616fd96de402006677fddb8a8af3573
Carl Swanson <cetswanson@gmail.com>
2015-05-26 23:21:36 -0400

    Update incorrect user sign up test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.feature "User signs up", :type => :feature do
 
-  scenario "A user signs up incorrectly" do
+  scenario "without valid credentials" do
     visit root_path
     click_on "signupButton"
 
     fill_in "signup_name", :with => "test"
-    fill_in "signup_password", :with => "test"
-    fill_in "signup_password_confirmation", :with => "not_test"
+    fill_in "signup_password", :with => "password"
+    fill_in "signup_password_confirmation", :with => "not_password"
 
-    expect{click_button "signupSubmit"}.not_to change(User, :count)
+    expect{click_on "signupSubmit"}.not_to change(User, :count)
   end
 end
