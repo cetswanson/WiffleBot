8a7229b4f6e8286562ea32bf8b5b4e5269a3fe9e
Carl Swanson <cetswanson@gmail.com>
2015-05-26 23:19:34 -0400

    Update new subforum test condition

--- spec/features/subforums_spec.rb
+++ spec/features/subforums_spec.rb
@@ -12,7 +12,8 @@ RSpec.feature "User creates a new subforum", :type => :feature do
     fill_in "subforum_title", :with => "test"
     fill_in "subforum_description", :with => "test"
 
-    expect{click_button "Submit"}.to change{Subforum.count}.by(1)
+    click_on "subforumSubmit"
+    expect(page).to have_text("test")
   end
 
   scenario "A user creates a new subforum without a title" do
