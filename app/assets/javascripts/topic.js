b7d44935f719506c7f13febee1df406c67f88fe7
Carl Swanson <cetswanson@gmail.com>
2015-05-27 16:37:12 -0400

    Refactor javascript for topics to work with turbolinks on page load

--- app/assets/javascripts/topic.js
+++ app/assets/javascripts/topic.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-    $("#newTopicButton").on('click', function(event) {
-      event.preventDefault();
-      if($( '#topic_create' ).is(":visible")){
-            $( '#topic_create' ).hide();
-       } else{
-            $( '#topic_create' ).show();
-       }
-   });
-   $(".topic").on('mouseover', function(event) {
-      $(this).css("background-color", "rgba(192,192,192, 0.8)")
-   });
-   $(".topic").on('mouseleave', function(event) {
-      $(this).css("background-color", "rgba(192,192,192, 0.6)")
-   });
-});
+var ready;
+ready = function() {
+
+  $("#newTopicButton").on('click', function(event) {
+    event.preventDefault();
+    if($( '#topic_create' ).is(":visible")){
+          $( '#topic_create' ).hide();
+     } else{
+          $( '#topic_create' ).show();
+     }
+  });
+
+  $(".topic").on('mouseover', function(event) {
+     $(this).css("background-color", "rgba(192,192,192, 0.8)")
+  });
+
+  $(".topic").on('mouseleave', function(event) {
+     $(this).css("background-color", "rgba(192,192,192, 0.6)")
+  });
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
