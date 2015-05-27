246a08069dad778198bcccd350ebb6121cfe7819
Carl Swanson <cetswanson@gmail.com>
2015-05-26 23:53:10 -0400

    Add on mouseover js for title

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+   $(".pageTitle").on('mouseover', function(event) {
+     event.preventDefault();
+     $(".pageTitle").css("color", "black");
+  });
     $(".pageTitle").on('click', function(event) {
       event.preventDefault();
       $(".userOptions").show()
