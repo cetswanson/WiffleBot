e41f00362f545c3b40e375dc3f8ad4eb851706ca
Carl Swanson <cetswanson@gmail.com>
2015-05-26 23:21:16 -0400

    Remove successful user sign up test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,16 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "User sign up", :type => :feature do
-  scenario "A user signs up correctly" do
-    visit root_path
-    click_on "signupButton"
-
-    fill_in "signup_name", :with => "test"
-    fill_in "signup_password", :with => "test"
-    fill_in "signup_password_confirmation", :with => "test"
-
-    expect{click_button "signupSubmit"}.to change{User.count}.by(1)
-  end
+RSpec.feature "User signs up", :type => :feature do
 
   scenario "A user signs up incorrectly" do
     visit root_path
