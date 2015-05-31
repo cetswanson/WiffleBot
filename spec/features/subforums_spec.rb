cf48f5145ab034c55c0945e4ff1f48b937ecbca4
Carl Swanson <cetswanson@gmail.com>
2015-05-30 23:55:43 -0400

    Add feature test for replying to a topic

---
+++ spec/features/subforums_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "User creates a new subforum", :type => :feature do
+
+  let!(:user) { User.create(name: "username", email: "cetswanson@gmail.com", password: 'password', password_confirmation: 'password')}
+  let!(:subforum) { Subforum.create(title: "title", description: "test", user_id: user.id) }
+
+  scenario "A user creates a new subforum correctly" do
+    page.set_rack_session(user_id: user.id)
+    visit subforums_path
+    click_on "Add a new Subforum"
+
+    fill_in "subforum_title", :with => "test"
+    fill_in "subforum_description", :with => "test"
+
+    click_on "subforumSubmit"
+    expect(page).to have_text("test")
+  end
+
+  scenario "A user creates a new subforum without a title" do
+    page.set_rack_session(user_id: user.id)
+    visit subforums_path
+    click_on "Add a new Subforum"
+
+    fill_in "subforum_description", :with => "test"
+
+    expect{click_on "subforumSubmit"}.not_to change(Subforum, :count)
+  end
+end
