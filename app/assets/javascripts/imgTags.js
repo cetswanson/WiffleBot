c8466d6af4832dd8b3ab3fc968f72b936ea34bf5
Carl Swanson <cetswanson@gmail.com>
2015-06-04 15:42:59 -0400

    Add addImg on enter javascript to add new input field

--- app/assets/javascripts/imgTags.js
+++ app/assets/javascripts/imgTags.js
@@ -1,6 +1,15 @@
 var ready;
 ready = function() {
 
+  $("#addImg").on('keypress', function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13){
+      var newField = '<input type="text" name="imgLink"></input>';
+      $(".replyImgs").append(newField);
+      $(".replyImgs").children().last().focus();
+    }
+  });
+};
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
