a07dc8f03276e401ce7fd291df57978ad830a15f
Carl Swanson <cetswanson@gmail.com>
2015-06-18 23:42:45 -0400

    Add mouseover effect for subforum index

--- app/assets/javascripts/subforum.js
+++ app/assets/javascripts/subforum.js
@@ -5,6 +5,11 @@ ready = function() {
 
   });
 
+  $(".subforum").on('mouseover', function(event) {
+     $(this).css("background-color", "rgba(192,192,192, 0.8)")
+  });
+
+};
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
