b9169bd5d7001dead2fa85635ec7eb2ad7642d41
Carl Swanson <cetswanson@gmail.com>
2015-06-28 16:17:25 -0400

    Add destroy method to replies controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -24,6 +24,14 @@ class RepliesController < ApplicationController
     @topic = Subforum.find(params[:topic_id])
   end
 
+  def destroy
+    @reply = Reply.find(params[:id])
+    @subforum = Subforum.find(params[:subforum_id])
+    @topic = Subforum.find(params[:topic_id])
+    @reply.destroy
+    redirect_to subforum_topic_path(@subforum, @topic)
+  end
+
   private
 
   def reply_params
